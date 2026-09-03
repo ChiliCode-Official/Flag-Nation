1f23e6d19d42aec38df46f035093e20646dfbaf9
ChiliCode <tilinrodrigo3@gmail.com>
2026-09-02 19:28:17 -0600

    FIREBASE

--- admin/firebase-config.js
+++ admin/firebase-config.js
@@ -3,7 +3,15 @@
  * de Firestore son las que protegen los datos. Pega aquí el objeto entregado
  * por Firebase Console > Project settings > Your apps > Web app.
  */
-export const firebaseConfig = null;
+export const firebaseConfig = {
+  apiKey: "AIzaSyCK0xZs52qEaGqY-lKobLJqvHN_u4NsWaQ",
+  authDomain: "flag-nation-mx.firebaseapp.com",
+  projectId: "flag-nation-mx",
+  storageBucket: "flag-nation-mx.firebasestorage.app",
+  messagingSenderId: "659602254296",
+  appId: "1:659602254296:web:6d077450effdbbc6ba2aad",
+  measurementId: "G-ZTFHJTP0Y7"
+};
 
 /** Correos autorizados como capa adicional de seguridad en la interfaz. */
 export const adminEmails = [];
